5347babcba1bfccdbe617b8584b894a3f25b723d
beforevocation <bjcystl@163.com>
2017-07-20 09:19:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+# coding: utf-8
 import download
 import bf
 
 def write_txt():
     htm = download.download('http://www.jcrb.com/')
-    htm2 = bf.bf(htm)                     #bf
+    htm2 = bf.bf(htm)
     fo = open("content.txt", "w")
-    fo.write(htm)
     fo1 = open("scrape.txt", "w")
+    fo.write(htm)
     fo1.write(htm2)
-    fo1.close()
     fo.close()
+    fo1.close()
+
 
